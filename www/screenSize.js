c411231782ea427468d6d01f13d763ca34494167
Jonasei <jonaseikli@gmail.com>
2014-08-05 15:44:58 +0200

    q

--- www/screenSize.js
+++ www/screenSize.js
@@ -2,9 +2,11 @@
     
     window.screenSize = function(str, callback) {
         cordova.exec(function(winParam) {
+        	console.log("SOMETHING WENT RIGHT");
         	console.log(winParam);
         	alert(winParam);
         }, function(err) {
+        	console.log("SOMETHING WENT WRONG");
             callback('Something went wrong.');
         }, "ScreenSize", "getSize", [str]);
     };    
