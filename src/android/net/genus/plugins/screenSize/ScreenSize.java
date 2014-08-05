4683efe1d0339de73bdc29b530ad1cf07ed10262
Jonasei <jonaseikli@gmail.com>
2014-08-05 15:33:44 +0200

    er

--- src/android/net/genus/plugins/screenSize/ScreenSize.java
+++ src/android/net/genus/plugins/screenSize/ScreenSize.java
@@ -1,52 +1,76 @@
- package org.apache.cordova.plugin;
+/*
+The MIT License (MIT)
 
-    import org.apache.cordova.CordovaPlugin;
-    import org.apache.cordova.CallbackContext;
+Copyright (c) 2014
 
-    import android.util.DisplayMetrics;
-    import android.view.WindowManager;
-    import android.content.Context;
-    import android.util.Log;
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-    import org.json.JSONArray;
-    import org.json.JSONException;
-    import org.json.JSONObject;
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-    /**
-     * This class returns the screen size in inches.
-     */
-    public class ScreenSize extends CordovaPlugin {
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
 
-        @Override
-        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-            if (action.equals("getSize")) {
+package net.genus.plugins.screensize;
 
-                DisplayMetrics metrics = new DisplayMetrics();
-                Context context = this.cordova.getActivity().getApplicationContext();;
-                WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
-                windowManager.getDefaultDisplay().getMetrics(metrics);
-               
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
 
-                int width=metrics.widthPixels;
-                int height=metrics.heightPixels;
-                int dens=metrics.densityDpi;
-                
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
+import android.content.Context;
+import android.util.Log;
 
-                double wi=(double)width/(double)dens;
-                double hi=(double)height/(double)dens;
-                double x = Math.pow(wi,2);
-                double y = Math.pow(hi,2);
-                double screenInches = Math.sqrt(x+y);
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-                Log.d("ScreenSizeDebug", ""+screenInches);
-                int result = (int) screenInches;
+/**
+ * This class returns the screen size in inches.
+ */
+public class ScreenSize extends CordovaPlugin  {
 
-                Log.d("ScreenSizeDebug", ""+result);
-                callbackContext.success(result);
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("getSize")) {
+            Log.d("ScreenSizeDebug", "_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-FINDING SCREEN SIZE _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-");
+            DisplayMetrics metrics = new DisplayMetrics();
+            Context context = this.cordova.getActivity().getApplicationContext();;
+            WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+            windowManager.getDefaultDisplay().getMetrics(metrics);
+           
 
-                
-                return true;
-            }
-            return false;
+            int width=metrics.widthPixels;
+            int height=metrics.heightPixels;
+            int dens=metrics.densityDpi;
+            
+
+            double wi=(double)width/(double)dens;
+            double hi=(double)height/(double)dens;
+            double x = Math.pow(wi,2);
+            double y = Math.pow(hi,2);
+            double screenInches = Math.sqrt(x+y);
+
+            Log.d("ScreenSizeDebug", ""+screenInches);
+            int result = (int) screenInches;
+
+            Log.d("ScreenSizeDebug", ""+result);
+            callbackContext.success(result);
+
+            
+            return true;
         }
+        return false;
     }
+}
