014c87a4a0e81a01fc165e57c93f85184806dfcf
Jonasei <jonaseikli@gmail.com>
2014-08-06 14:27:50 +0200

    7

--- www/screenSize.js
+++ www/screenSize.js
@@ -4,18 +4,20 @@ var exec = require('cordova/exec');
 
 var screenSize = function(window) {
 
-	window.screenSize = function(str, callback) {
-		exec(function(result) {
-				callback(result);
-			}, 
-			function(err) {
-		    	console.log("SOMETHING WENT WRONG");
-		        callback('Something went wrong.');
-		    }, 
-		    "ScreenSize", 
-		    "getSize", 
-		    [str]);
-	}  
+	(function(str, callback) {
+  		exec(function(result) {
+  				window.screenSize = result;
+  				callback(result);
+  			}, 
+  			function(err) {
+  		    	console.log("SOMETHING WENT WRONG");
+  		        callback('Something went wrong.');
+  		    }, 
+  		    "ScreenSize", 
+  		    "getSize", 
+  		    [str]
+  		);
+  	})(window)  
 }
 screenSize(window);
 
